3a0cb75992a414281bd2a6b418a87f5531f67164
Vasavi243 <bhoomireddyvasavi0@gmailcom>
2026-03-28 18:03:44 +0530

    Fix: Add LoginManager to resolve current_user error

--- app.py
+++ app.py
@@ -30,3 +30,13 @@ with app.app_context():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+from flask_login import LoginManager
+from models import User
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
